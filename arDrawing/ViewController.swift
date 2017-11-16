fa919f8c421a42045790d75c1734c687b4e75f8c
Joe Hill <joe@jkh3.com>
2017-11-15 23:53:42 -0500

    Drawing on your iPhone in Augmented Reality, using ARKit and ARSCNVIEW

---
+++ arDrawing/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  arDrawing
+//
+//  Created by Joe Hill on 11/15/17.
+//  Copyright © 2017 Joe Hill. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.showsStatistics = true
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        print("rendering")
+    }
+
+
+}
+
